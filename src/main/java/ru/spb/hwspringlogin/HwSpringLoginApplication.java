053c7a634c085beffe6d154745d94303f44b6075
Andrew <skat2006@bk.ru>
2022-07-31 19:17:25 +0300

    init

---
+++ src/main/java/ru/spb/hwspringlogin/HwSpringLoginApplication.java
@@ -0,0 +1,13 @@
+package ru.spb.hwspringlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpringLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwSpringLoginApplication.class, args);
+	}
+
+}
